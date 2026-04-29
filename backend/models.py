a355d2fb1956add9a47f92ec1e2c5ffab6655f85
cthomas200 <chynadthomas@gmail.com>
2026-04-29 15:53:40 -0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='customer')  # customer or provider
     bio = db.Column(db.Text, nullable=True)
     image = db.Column(db.String(255), nullable=True)
